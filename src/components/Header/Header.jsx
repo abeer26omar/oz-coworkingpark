24750d69e72c2d63f8841c6e9a3b2a2f5e0e3dba
Ahmed Ashraf <ahmedashraf2398@gmail.com>
2023-05-19 19:21:29 +0300

    fix header responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,59 +1,58 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Header.css';
 import logo from '../../assets/images/logo.svg';
 import loginIcon from '../../assets/images/icons/loginIcon.png';
 
-import {NavLink} from "react-router-dom";
-import {Nav} from "react-bootstrap";
+// import {Nav} from "react-router-dom";
+import {Nav, Container} from "react-bootstrap";
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 const Header = () => {
+    const [showBasic, setShowBasic] = useState(false);
     return (
         <>
-        <Nav className="navbar navbar-expand-lg ">
-            <div className="container-fluid">
-                <NavLink className="navbar-brand" target="_blank" to="#">
+        <Navbar expand="lg">
+            <Container fluid >
+                <Nav.Link className="navbar-brand" target="_blank" to="/">
                     <img src={logo} className="w-75" alt={logo}/>
-                </NavLink>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse justify-content-between" id="navbarNav">
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
-                            <NavLink
-                               className="nav-link active" >About OZ</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink style={({ isActive }) => ({
-                                color: isActive ? '#' : '#',
-                            })} className="nav-link" to={"/"}>Venues</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink
-                              className="nav-link">Community</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink style={({ isActive }) => ({
-                                color: isActive ? '#ffc107' : '#818181',
-                            })} className="nav-link" to={"/membership"}>Membership</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className="nav-link">F & B</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink  className="nav-link" >Events</NavLink>
-                        </li>
-                    </ul>
-                    <div className="d-flex">
-                        <NavLink style={({ isActive }) => ({
-                            color: isActive ? '#ffc107' : '#818181',
-                        })} className="nav-link navbar-text mx-4" to={"/contactus"}>inquire</NavLink>
-                        <NavLink style={({ isActive }) => ({
-                            color: isActive ? '#ffc107' : '#818181',
-                        })} className="nav-link m-auto" to={"/"}>Login <img src={loginIcon}/></NavLink>
+                </Nav.Link>
+                <Navbar.Toggle aria-controls="basic-navbar-nav"   onClick={() => setShowBasic(!showBasic)}  />
+                <Navbar.Collapse  id="basic-navbar-nav"  show={showBasic} >
+                    <Nav className="m-auto">
+                                <Nav.Link className="nav-link active" >About OZ</ Nav.Link>
+                                <Nav.Link className="nav-link" to={"/"}>Venues</ Nav.Link>
+                                <Nav.Link className="nav-link">Community</ Nav.Link>
+                                <Nav.Link   className="nav-link" to={"/membership"}>Membership</ Nav.Link>
+                                <Nav.Link className="nav-link">F & B</ Nav.Link>
+                                <Nav.Link  className="nav-link" >Events</ Nav.Link>
+
+                    </Nav>
+                    <div className="d-flex justify-content-end">
+                        <Nav.Link className="nav-link navbar-text mx-4" to={"/contactus"}>inquire</ Nav.Link>
+                        <Nav.Link className="nav-link m-auto" to={"/"}>Login<img className="" src={loginIcon}/> </ Nav.Link>
                     </div>
-                </div>
-            </div>
-        </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+
+
+            {/*<Navbar.Brand className="w-75">*/}
+            {/*    <Nav.Link className="navbar-brand" target="_blank" to="#">*/}
+            {/*        <img src={logo} className="w-75" alt={logo}/>*/}
+            {/*    </ Nav.Link>*/}
+            {/*</Navbar.Brand>*/}
+            {/*<Navbar.Toggle aria-controls="basic-navbar-nav" />*/}
+            {/*<Navbar.Collapse id="basic-navbar-nav">*/}
+            {/*    <Nav className="me-auto">*/}
+            {/*        <Nav.Link style={({ isActive }) => ({*/}
+            {/*            color: isActive ? '#' : '#',*/}
+            {/*        })} className="nav-link" to={"/"}>About</Nav.Link>*/}
+            {/*        <Nav.Link style={({ isActive }) => ({*/}
+            {/*            color: isActive ? '#' : '#',*/}
+            {/*        })} className="nav-link" to={"/"}>About</Nav.Link>*/}
+
+            {/*    </Nav>*/}
+            {/*</Navbar.Collapse>*/}
         </>
     );
 };
