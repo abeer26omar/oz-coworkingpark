5d77b29ae45c35435b9eb3eadfee972638d4b791
Ahmed Ashraf <ahmedashraf2398@gmail.com>
2023-08-10 16:53:10 +0300

    add fav options

---
+++ src/components/Community/CommunityEvents/CommunityEventsExplore/CommunityEventsSliders/MultipleEventSlider.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import Slider from "react-slick";
+import {CommunityEventsData} from "../../../../../Data/CommunityEventsData";
+import SingleCommunityExplore from "../SingleCommunityExplore";
+
+const MultipleEventSlider = () => {
+    const settings = {
+        className: "center",
+        dots: false,
+        infinite: true,
+        arrows: false,
+        autoplay: false,
+        autoplaySpeed: 3000,
+        rows: 2,
+        slidesPerRow: 2,
+        vertical: true,
+        verticalSwiping: true,
+        // centerMode: true,
+        infinite: true,
+        pauseOnHover: true,
+        swipeToSlide: true,
+        easing: "ease-in-out",
+        duration: 3000,
+        useTransform: true,
+        lazyLoad: true,
+        adaptiveHeight: true,
+
+
+        // adaptiveHeight: true,
+        beforeChange: function (currentSlide, nextSlide) {
+            console.log("before change", currentSlide, nextSlide);
+        },
+        afterChange: function (currentSlide) {
+            console.log("after change", currentSlide);
+        }
+    };
+
+    return (
+        <>
+
+            <div className="slider-vertical-rows">
+                <Slider {...settings}>
+                    {CommunityEventsData.map((eventData, index) => {
+                        const {id, img, linkText, title, text, category} = eventData;
+                        return (
+                            <div key={index}>
+                                <SingleCommunityExplore id={id} img={img} linkText={linkText}
+                                                        title={title}
+                                                        text={text} category={category}/>
+                            </div>
+                        )
+                    })}
+                </Slider>
+
+            </div>
+
+
+        </>
+    );
+};
+
+export default MultipleEventSlider;
