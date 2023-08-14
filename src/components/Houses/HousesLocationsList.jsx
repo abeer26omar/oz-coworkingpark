d2fff0aabc2c37a4bd7a5e97fdb3943b319a3715
Ahmed Ashraf <ahmedashraf2398@gmail.com>
2023-08-14 16:15:30 +0300

    Deploy to GitHub Pages

---
+++ src/components/Houses/HousesLocationsList.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+
+const HousesLocationsList = ({id, img, government, address}) => {
+    return (
+        <>
+
+            <div className="box-office" key={id}>
+                <div className="d-flex justify-content-between align-items-center">
+                    <div className="location-contact">
+                        <h2 className="h2-text-box">{address}</h2>
+                        <span className="government">{government}</span>
+                    </div>
+                    <div className="btn-explore">
+                        <Link to={`/houses/${id}`} href="#" className="btn button-outLine btn-bg-white ">Explore</Link>
+                    </div>
+                </div>
+                <img src={img} alt="office" className="w-100"/>
+
+
+            </div>
+
+        </>
+    );
+};
+
+export default HousesLocationsList;
