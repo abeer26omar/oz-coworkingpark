bf5d4c7eefa4f2e95b12b1b73577d762229791da
abeer26omar <abeer.omar504@gmail.com>
2023-12-02 20:39:38 +0200

    modify: about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,12 +2,14 @@ import React, { useContext, useEffect } from "react";
 import AboutHeader from "../components/About/AboutHeader/AboutHeader";
 import AboutOz from "../components/About/AboutOz/AboutOz";
 import { DataContext } from "../apis/context/SiteDataContext";
+
 const About = () => {
 
-    const { config, getComponentValue } = useContext(DataContext);
-  useEffect(() => {
-    config("about_us");
-  }, []);
+    const { ResetPageName, getComponentValue } = useContext(DataContext);
+    useEffect(() => {
+      ResetPageName('about_us')
+
+    }, []);
 
   return (
     <>
